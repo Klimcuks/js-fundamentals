32514de4004eaa52415aca6a9b859ffbec5f2916
Nikita Klimcuks <nikita.klimchuck@gmail.com>
2022-07-10 14:21:26 +0100

    assignment completed

--- src/conditional-flow/multiple-conditions.js
+++ src/conditional-flow/multiple-conditions.js
@@ -6,14 +6,16 @@ const NUM = 9
 // Use conditional statements to set the value of the answerOne variable
 // to be true if the NUM variable is more than or equal to the LOWER variable
 // AND is less than or equal to the UPPER variable
-let answerOne
+let answerOne = true                                                            // NOT SURE ABOUT THIS ONE//
 
-
+if (NUM >= LOWER && NUM <= UPPER) {
+  answerOne = true
+}
 
 // ---- //
 
 // Task 2
-const STR = null
+const STR = 'yoyo'
 
 // Use conditional statements to set the answerTwo variable below to true
 // if the STR variable is 'Hello' or 'Goodbye'
@@ -22,12 +24,17 @@ const STR = null
 // to verify your code is correct
 let answerTwo
 
-
+if (STR === 'Hello' || STR === 'Goodbye') {
+  answerTwo = true
+  console.log('checki if answerTwo is', answerTwo)
+} else if (STR !== 'Hello' && STR !== 'Goodbye') {
+  answerTwo = false
+  console.log('check if answerTwo is', answerTwo)
+}
 
 // ---- //
 
 // Task 3
-const AGE = 0
 
 // Use conditional statements to set the answerThree variable below to a
 // string value based on what the AGE variable is set to.
@@ -41,8 +48,26 @@ const AGE = 0
 // 5-12   | Child
 // 13-19  | Teenager
 // 20+    | Adult
-let answerThree
 
+const AGE = 14
+let answerThree = ''
+
+if (AGE === 0) {
+  answerThree = 'Baby'
+  console.log('answerThree is', answerThree)
+} else if (AGE < 5) {
+  answerThree = 'Toddler'
+  console.log('answerThree is', answerThree)
+} else if (AGE <= 12) {
+  answerThree = 'Child'
+  console.log('answerThree is', answerThree)
+} else if (AGE <= 19) {
+  answerThree = 'Teenager'
+  console.log('answerThree is', answerThree)
+} else if (AGE >= 20) {
+  answerThree = 'Adult'
+  console.log('answerThree is', answerThree)
+} else consoe.log('try again')
 
 // Run the test after changing the AGE value to verify you've successfully
 // accounted for each age range
--- src/conditional-flow/numeric-conditions.js
+++ src/conditional-flow/numeric-conditions.js
@@ -5,12 +5,23 @@ const ARRAY_TWO = ['Hello', 'Conditions']
 
 // Use conditional statements to set answerOne to false if ARRAY_ONE is not empty
 // or true if it is empty
+
 let answerOne
+if (ARRAY_ONE) {
+  answerOne = true
+} else {
+  answerOne = false
+}
 
 // Use conditional statements to set answerTwo to false if ARRAY_TWO is not empty
 // or true if it is empty
 let answerTwo
 
+if (ARRAY_TWO) {
+  answerTwo = false
+} else {
+  answerTwo = true
+}
 
 
 const NUM_ONE = 92837
@@ -18,6 +29,12 @@ const NUM_TWO = 92827
 
 // Use conditional statements to set answerThree to true if NUM_ONE is more than NUM_TWO
 let answerThree
+
+if (NUM_ONE > NUM_TWO) {
+    let answerThree = true 
+} else {
+    let answerThree = false
+}
 
 
 
