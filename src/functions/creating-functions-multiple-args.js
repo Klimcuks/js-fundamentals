dec147cf7e107ac2ed9856ca5b876e01f61f6f84
Nikita Klimcuks <nikita.klimchuck@gmail.com>
2022-07-12 17:58:11 +0100

    assignment completed

--- src/functions/creating-functions-multiple-args.js
+++ src/functions/creating-functions-multiple-args.js
@@ -9,6 +9,14 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function allTheNums(lower, upper) {
+  const myArray = []
+  for (let i = lower; i <= upper; i++) {
+    myArray.push(i)
+  }
+  return myArray
+}
+console.log(allTheNums(3, 8))
 
 // define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -24,6 +32,6 @@
 
 // change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  a: allTheNums, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
   b: undefined // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
 }
